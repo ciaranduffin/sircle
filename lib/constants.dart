b286b9d4fa94ad36b9887e576ffa4dbab1adeace
ciaranduffin <52404181+ciaranduffin@users.noreply.github.com>
2021-10-21 23:44:31 +0100

    Sign in screen ui

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColourPalette {
+  static const ParadisePink = const Color(0xFFEF476F);
+  static const OrangeYellowCrayola = const Color(0xFFFFD166);
+  static const CaribbeanGreen = const Color(0xFF06D6A0);
+  static const BlueNCS = const Color(0xFF118AB2);
+  static const MidnightEagleGreen = const Color(0xFF073B4C);
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:sircle/constants.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -15,29 +16,46 @@ class _HomeState extends State<Home> {
 
   Scaffold buildUnAuthScreen() {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Sircle',
-            style: TextStyle(
-                fontFamily: "Signatra", fontSize: 60.0, color: Colors.white),
-          ),
-        ),
-        body: Container(
-          child: Column(
-            children: [
-              GestureDetector(
-                  child: Container(
-                width: 260,
-                height: 60,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/google_signin_button.png'),
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          colors: [
+            ColourPalette.BlueNCS,
+            ColourPalette.CaribbeanGreen,
+            // ColourPalette.MidnightEagleGreen,
+            ColourPalette.OrangeYellowCrayola,
+            ColourPalette.ParadisePink,
+          ],
+        )),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'Sircle',
+              style: TextStyle(
+                  fontFamily: "Signatra", fontSize: 60.0, color: Colors.white),
+            ),
+            GestureDetector(
+                onTap: () => print('ontap'),
+                child: Container(
+                  width: 260,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image:
+                          AssetImage('assets/images/google_signin_button.png'),
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                ),
-              )),
-            ],
-          ),
-        ));
+                )),
+          ],
+        ),
+      ),
+    );
   }
 
   @override
