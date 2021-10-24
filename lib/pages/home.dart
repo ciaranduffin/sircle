a59d640ffd4aec6b5988b576156eda0a664bac2f
ciaranduffin <52404181+ciaranduffin@users.noreply.github.com>
2021-10-25 00:00:16 +0100

    tabs added

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,6 +2,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sircle/constants.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:sircle/pages/activity_feed.dart';
+import 'package:sircle/pages/profile.dart';
+import 'package:sircle/pages/search.dart';
+import 'package:sircle/pages/timeline.dart';
+import 'package:sircle/pages/upload.dart';
 
 final GoogleSignIn googleSignIn = GoogleSignIn();
 
@@ -12,10 +17,14 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   bool isAuth = false;
+  late PageController pageController;
+  int pageIndex = 0;
 
   @override
   initState() {
-    super.initState();
+    pageController = PageController();
+
+    // Detects when user is signed in or out
     googleSignIn.onCurrentUserChanged.listen((account) {
       handleSignIn(account);
     }, onError: (err) {
@@ -27,6 +36,14 @@ class _HomeState extends State<Home> {
         .signInSilently(suppressErrors: false)
         .then((account) => handleSignIn(account))
         .catchError((err) => print('Error signing in: $err'));
+
+    super.initState();
+  }
+
+  @override
+  dispose() {
+    pageController.dispose();
+    super.dispose();
   }
 
   handleSignIn(GoogleSignInAccount? account) {
@@ -51,10 +68,53 @@ class _HomeState extends State<Home> {
     googleSignIn.signOut();
   }
 
-  Widget buildAuthScreen() {
+  onPageChanged(int pageIndex) {
+    setState(() {
+      this.pageIndex = pageIndex;
+    });
+  }
+
+  onBottomNavTap(int pageIndex) {
+    pageController.jumpToPage(pageIndex);
+  }
+
+  Scaffold buildAuthScreen() {
     return Scaffold(
-      appBar: AppBar(),
-      body: Text('Authenticated'),
+      appBar: AppBar(
+        title: Text(
+          'Sircle',
+          style: TextStyle(
+            fontFamily: "Signatra",
+            fontSize: 40.0,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: PageView(
+        children: [
+          Timeline(),
+          ActivityFeed(),
+          Upload(),
+          Search(),
+          Profile(),
+        ],
+        controller: pageController,
+        onPageChanged: onPageChanged,
+        // physics: NeverScrollableScrollPhysics(),
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        currentIndex: pageIndex,
+        onTap: onBottomNavTap,
+        activeColor: Theme.of(context).primaryColor,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.whatshot_outlined)),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.notifications_active_outlined)),
+          BottomNavigationBarItem(icon: Icon(Icons.photo_camera_outlined)),
+          BottomNavigationBarItem(icon: Icon(Icons.search_outlined)),
+          BottomNavigationBarItem(icon: Icon(Icons.account_circle_outlined)),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: logout,
         child: Icon(Icons.logout),
